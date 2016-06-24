e2f3adc80650b13beb3fee6ed26caf13949d5114
Frode Nilsen <frode.nilsen@vg.no>
2016-06-24 15:43:19 +0200

    Improve readability of rx pipleine code

--- pipeline/rx.java
+++ pipeline/rx.java
@@ -1,13 +1,12 @@
 Observable<String> obs1 = Observable.just("Hello world");
-    obs1.flatMap(s -> {
-        Observable<Integer> obs2 =  Observable.just(s.length());
-      return obs2;
-    }).flatMap(integer -> {
-        Observable<String> obs3 = Observable.just(
-            "Length of first string: " + integer);
-        return obs3;
-    }).subscribe(s ->{
-        System.out.println(s);
-        // Will output "Length of first string: 11"
-    });
+obs1.flatMap(s -> {
+    Observable<Integer> obs2 =  Observable.just(s.length());
+    return obs2;
+}).flatMap(i -> {
+    Observable<String> obs3 = Observable.just("Length: " + i);
+    return obs3;
+}).subscribe(s ->{
+    System.out.println(s);
+    // Will output "Length: 11"
+});
 
