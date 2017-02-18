017351a0acee4b7c47c4aedca34ff6b619b74cb5
Frode Nilsen <nilzor@gmail.com>
2017-02-18 11:09:09 +0100

    Added Kotlin language to the PHP export script and make it HTML encode it

--- export-html-tabs.php
+++ export-html-tabs.php
@@ -1,17 +1,16 @@
 <?php
-$patternHeader = "h3";
-$subHeader = "h4";
 $codeClass = "";
 $langDefList =  json_decode(file_get_contents("languageDefinitions.json"));
 $index = json_decode(file_get_contents("index.json"));
 
-$filter = ["es6-promise.js", "rx.java", "csharp.cs"];
+$filter = ["es6-promise.js", "rx.java", "csharp.cs", "kotlin-co.kt"];
 
 $i = 0;
 $j = 0;
 foreach ($index as $pattern) {
     $i++;
-    echo ("<p><$patternHeader>" . $pattern->patternName . "</$patternHeader>\n");
+    echo ("\n<!-- PATTERN START: --->\n");
+    echo ("<p><h3 style='display:inline-block'>" . $pattern->patternName . "</h3>\n");
     $groupName = "group" . $i;
     echo "<div class=\"tabbed\">";
     $checked = "checked";
@@ -22,18 +21,21 @@ foreach ($index as $pattern) {
         $fullPath = $pattern->path . "/" . $langDef->filename;
         if (!file_exists($fullPath)) continue;
         $code = file_get_contents($fullPath);
+				$code = str_replace("<", "&lt;", $code);
+				$code = str_replace(">", "&gt;", $code);
 
         // Output
         $tabId = "tabbed$j";
         $tabTitle = $langDef->name;
         $langDefName = $langDef->wordPressLangName;
         ?>
-
             <input name='<?=$groupName?>' id='<?=$tabId?>' type='radio' <?=$checked?>>
-            <section><h5><label for='<?=$tabId?>'><?=$tabTitle?></label></h5>
-                <div><pre class="brush:<?=$langDefName?>">
-<?=$code?>
-</pre> </div></section>
+            <section>
+                <h5><label for='<?=$tabId?>'><?=$tabTitle?></label></h5>
+                <div>
+                    <pre><code class="<?=$langDefName?>"><?=$code?></code></pre>
+                </div>
+            </section>
         <?php
         $checked = "";
     }
